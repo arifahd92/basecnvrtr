639f445050263009c0ec45ff58d5c5175888cbdb
md arif <arifahd92@gmail.com>
2023-03-10 23:12:12 +0530

    error handling

--- base.js
+++ base.js
@@ -7,15 +7,29 @@ let frmbase= document.querySelector("#frmbase")
 btn.addEventListener("click",function(){
   let frmbase= document.querySelector("#frmbase")
  let sel = +frmbase.value
+ var error=false
   // console.log(typeof(sel))// number
 if(sel ===10){
+  let inptarr=[...inpt.value]//array will contain value in string form like["1","4",'h']
+  inptarr.forEach((e)=>{
+    console.log(e)
+    if(typeof(e) === "string" && isNaN(e) && e!="."){
+      error=true
+      console.log("error detected")
+    
+    }
+  })
   let num = +inpt.value
-  console.log(num)
+  
   let v = +tobase.value
-  console.log(v)
-  if (inpt.value===""){
-      res.innerText="result: please enter value"  
+  // console.log(v)
+   if (inpt.value===""){
+       res.innerText="result: please enter value"  
+   }
+  else if(error){
+    res.innerText="result: please enter valid number"  
   }
+
  else{
   let actres=num.toString(v)
   if(v==16){
@@ -30,13 +44,35 @@ if(sel ===10){
 else{
   let inputvalue= inpt.value;
   // type of inputvalue is string
+  let inptarr=[...inpt.value]
+  if(+frmbase.value<16){
+      inptarr.forEach((e)=>{
+        if(e>=+frmbase.value){
+          error=true
+          console.log("error found in input")
+        }
+        else if(typeof(e) === "string" && isNaN(e)&& e!="."){
+          error=true
+        }
+      })
+  }
+  else if(+frmbase.value==16){
+    const hexRegex = /^[0-9A-Fa-f]+$/g;
+    if (!hexRegex.test(inpt.value)) {
+      console.log('Invalid hexadecimal number');
+      error=true
+    }
+  }
    let dec2 =   parseInt(inputvalue,+frmbase.value)
-     //decimal equivalent of input at from base
+     // dec2 is decimal equivalent of input at from base
    let num = dec2
   let v = +tobase.value//+operator for string to no.
-  console.log(isNaN(parseFloat(num)))
+ console.log(isNaN(parseFloat(num)))
   if(isNaN(parseFloat(num))){
     res.innerText="result: "+"Enter valid no. and make sure the no. exist at selected base"
+  }
+  else if(error){
+    res.innerText="result: "+"please enter valid number"
   }
   else
   res.innerText="result: "+num.toString(v)
